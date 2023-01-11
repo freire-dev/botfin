f5182489012a3476e814e359e0154b39ca7019d5
Luan Freire <lunfreire@projectbox.pt>
2023-01-11 00:26:06 -0300

    acréscimo de fluxo do chat bot com reply keyboard.

--- sigma.py
+++ sigma.py
@@ -1,10 +1,12 @@
 import requests
+import json
+import urllib.parse
 
 class SigmaFinBot:
 
     def __init__(self):
 
-        self.token = '5948859286:AAEPwfdudgTNad-Y2OZmVmd1nuOHNqzK2sk'
+        self.token = '5826602075:AAEHbmyCiJ1AilxXiGYNyKg3XmIdc87lzkA'
         self.urlBase = f"https://api.telegram.org/bot{self.token}/"
 
     def enviarMensagem(self, idChat, mensagem):
@@ -13,9 +15,22 @@ class SigmaFinBot:
         link_post = f'{self.urlBase}sendMessage?chat_id={chatId}&text={mensagem}'
         requests.post(link_post)
 
+    def enviarMensagemGuiada(self, idChat, texto, buttonsList):
+
+        print(buttonsList)
+        chatId = idChat
+        btnObject = {"keyboard": [buttonsList], "one_time_keyboard": True, "resize_keyboard": True, "is_persistent": False}
+        buttonsOptions = json.dumps(btnObject)
+        buttonsOptions = urllib.parse.quote(f"{buttonsOptions}".encode('utf8'))
+        link_post = f'{self.urlBase}sendMessage?chat_id={chatId}&text={texto}&reply_markup={buttonsOptions}'
+        requests.post(link_post)
+
     def obterResultados(self):
 
         resp = requests.get(self.urlBase + "getUpdates")
         json = resp.json()['result'][-1]
-        print('Resposive 200', json)
+        try:
+            print(f"Respose [200] | ID User: {json['message']['from']['id']} ----> Message: {json['message']['text']} ")
+        except:
+            print('Response [200]')
         return json
